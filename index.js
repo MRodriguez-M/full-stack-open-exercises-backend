cf0d6ccc93b4954324a335f0000ea101de9b2e49
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2024-08-13 16:07:00 -0600

    Exercise 3.7 complete

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
-app.use(express.json())
+const morgan = require('morgan');
+
+app.use(express.json());
+app.use(morgan('tiny'));
 
 let persons = [
     { 
