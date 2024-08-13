81e7e91cf2ca9a1b3549c89a82f8fea08ce07379
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2024-08-13 16:44:44 -0600

    Exercise 3.8 complete

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const PORT = 3001;
 const morgan = require('morgan');
 
+morgan.token('data', (req, res) => {
+  return JSON.stringify(req.body);
+});
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
 let persons = [
     { 
