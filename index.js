315895d14180f7d2fdf96dfe3f1f87c72f24df2a
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2025-05-30 21:55:25 -0600

    Exercise 3.9 complete

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const morgan = require('morgan');
+const cors = require('cors');
+app.use(cors());
 
 morgan.token('data', (req, res) => {
   return JSON.stringify(req.body);
