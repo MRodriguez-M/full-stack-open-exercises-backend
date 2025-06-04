4f8dddab435b174d8c9e398f4b422dedd5c1c764
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2025-06-03 21:54:15 -0600

    Exercise 3.11 complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.static('dist'))
 const PORT = process.env.PORT || 3001;
 const morgan = require('morgan');
 const cors = require('cors');
